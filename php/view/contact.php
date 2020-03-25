d75ac07c336cce2b1b64c3c46697c32dffde3175
melodiesals <melodie.salsano@outlook.fr>
2020-03-25 15:32:32 +0100

    enlever html

--- php/view/contact.php
+++ php/view/contact.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<h1>Biscottes</h1>
-<body>
-    
-</body>
-</html>
-
-</main>
-
 <h2>Biskot est là pour répondre à vos questions</h2>
 <form name="bisckotform" action="traitementcontact.php" method="post">
    <p> Nom :<br/>
