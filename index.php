04bfbd914ec0a8af192b424dabf2407d835d3770
MaximeB139 <maxbragh@gmail.com>
2020-03-25 12:16:12 +0100

    h

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 
 require_once "php/view/presentation.php";
 require_once "php/view/section-produits.php";
+require_once "php/view/points-ventes.php";
---
+++ php/view/points-ventes.php
@@ -0,0 +1,12 @@
+<section>
+        <h2>Nos Points De Ventes</h2>
+
+        <div style="position:relative; height:400px">
+            <div style="position:absolute;z-index:1">
+               <img src="assets/img/carte1.jpg" alt="carte france">
+               <img src="assets//img/carte2.png" alt="carte USA">
+            </div>
+            <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
+            </div> 
+        </div>
+        </section>
