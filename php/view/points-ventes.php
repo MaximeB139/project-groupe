553ca60c8a44b4a5274cc5e4c4299a8657574298
Widka <Weedka13@gmail.com>
2020-03-25 15:39:02 +0100

    changement du code

--- php/view/points-ventes.php
+++ php/view/points-ventes.php
@@ -2,11 +2,11 @@
         <h2>Nos Points De Ventes</h2>
 
         <div style="position:relative; height:400px">
-            <div style="position:absolute;z-index:1">
+            <div style="position:absolute; z-index:1; height:100%">
                <img src="assets/img/carte1.jpg" alt="carte france">
                <img src="assets//img/carte2.png" alt="carte USA">
             </div>
-            <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
-            </div> 
+            <!-- <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
+            </div> --> 
         </div>
-        </section>
+</section>
