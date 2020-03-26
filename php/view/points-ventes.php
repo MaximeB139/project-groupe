eb5ee4391bf1083111964a9e3b490c4bebfc247f
MaximeB139 <maxbragh@gmail.com>
2020-03-26 09:57:27 +0100

    hfhf

--- php/view/points-ventes.php
+++ php/view/points-ventes.php
@@ -1,20 +1,14 @@
 <section>
         <h2><div id="Nos Points De Ventes"> Points De Ventes</div></h2>
 
-<<<<<<< HEAD
         <!--<div style="position:relative; height:400px">
             <div style="position:absolute;z-index:1">
-=======
-        <div style="position:relative; height:400px">
-            <div style="position:absolute; z-index:1; height:100%">
->>>>>>> 500e7517504662cdedc68015b5daa540a7b4d502
                <img src="assets/img/carte1.jpg" alt="carte france">
                <img src="assets//img/carte2.png" alt="carte USA">
             </div>
             <!-- <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
             </div> --> 
         </div>
-<<<<<<< HEAD
         </section><!-->
 
         <div class="container"> 
@@ -31,6 +25,3 @@
     }
 ?>    
 </div>
-=======
-</section>
->>>>>>> 500e7517504662cdedc68015b5daa540a7b4d502
