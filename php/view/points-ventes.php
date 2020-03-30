4508f191f1963018daa224464c458df1338b94d2
MaximeB139 <maxbragh@gmail.com>
2020-03-30 14:55:11 +0200

    g maps

--- php/view/points-ventes.php
+++ php/view/points-ventes.php
@@ -6,22 +6,24 @@
                <img src="assets/img/carte1.jpg" alt="carte france">
                <img src="assets//img/carte2.png" alt="carte USA">
             </div>
-            <!-- <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
-            </div> --> 
-        </div>
-        </section><!-->
+         <div style="position:absolute;top:360px; width:600px; height:400px; z-index:2;font-size:200%">
+            </div> -->
+            --------------------------------------------------------------------------------------------------
+            <!--
+        </section>
 
-        <div class="container"> 
+        <div class="container"> -->
 <?php
-    $tableau    = [ "carte1.jpg", "carte2.png"];
-    $tableauAlt = ["alt1", "alt2" ];
-    for($i=0; $i < count($tableau); $i++)
-    {
-        echo
-    <<<CODEHTML
-                <img src="assets/img/$tableau[$i] " alt="$tableauAlt[$i]">
+    //$tableau    = [ "carte1.jpg", "carte2.png"];
+    //$tableauAlt = ["alt1", "alt2" ];
+    //for($i=0; $i < count($tableau); $i++)
+    //{
+     //   echo
+    //<<<CODEHTML
+                //<img src="assets/img/$tableau[$i] " alt="$tableauAlt[$i]">
      
-    CODEHTML;    
-    }
+    //CODEHTML;    
+    //}
 ?>    
-</div>
+</div> 
+<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1mLhAIUgZsWracNS8qZgrTOL70RyQh8Lg" width="640" height="480"></iframe>
