629ffef29ce8d80cd5ff9453fd2fd03150c0a77d
Widka <Weedka13@gmail.com>
2020-03-30 15:25:54 +0200

    modif section produit

--- php/view/section-produits.php
+++ php/view/section-produits.php
@@ -15,7 +15,7 @@
         <section id="produits">
             <h2><div id="Nos produits">Nos produits</div></h2>
                 <p>Du petit déjeuner au goûter, en passant par le dessert, nos produits vous accompagne dans toutes vos envies gourmandes.</p>
-                <img class="imagePrincipal" src="assets/img/Biscottes.png" alt="Types de Biscottes">
+                <img class="imagePrincipale" src="assets/img/Biscottes.png" alt="Types de Biscottes">
                 <div class="container">
          
 
