67a2401b3d09685d1963888a8ec7aedd448a17d7
MaximeB139 <maxbragh@gmail.com>
2020-04-06 14:59:48 +0200

    VF

--- php/view/points-ventes.php
+++ php/view/points-ventes.php
@@ -1,4 +1,8 @@
 <section>
-        <h2><div id="Nos Points De Ventes"> Points De Ventes</div></h2>    
-</div> 
-<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1mLhAIUgZsWracNS8qZgrTOL70RyQh8Lg" width="640" height="480"></iframe>
+        <h2><div id="Nos Points De Ventes"> Points De Ventes</div></h2>   
+        
+        <p>Nos conseillés présent dans nos différents points de ventes, implantés dans les plus grandes villes d'Europe, se tiennent a votre entiére disposition pour touts conseils et questions.
+                
+            </div> 
+    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1mLhAIUgZsWracNS8qZgrTOL70RyQh8Lg" width="640" height="480"></iframe>
+</section>
