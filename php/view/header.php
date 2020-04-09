6b4c90de7c50f92a1ca1b0cea0fbfbe7c9bb6038
melodiesals <melodie.salsano@outlook.fr>
2020-04-09 16:26:26 +0200

    href formulaire co

--- php/view/header.php
+++ php/view/header.php
@@ -14,7 +14,7 @@
 <a href="#Nos produits">Nos produits</a>
 <a href="#Nos Points De Ventes">Nos Points De Ventes</a>
 <a href="#contact">contact</a>
-<a href="#formulairedeconnexion" src= "formulairedeconnexion.html">Connexion</a>
+<a href="formulairedeconnexion.html">Connexion</a>
 </section>
     <img class="logo" src="assets/img/LogoBiskot.png" alt="logo entreprise">
     
