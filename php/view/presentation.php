f2a04eec04d2195535ec0b9064c9ec50b4162d3c
Bmars <bapt_13@hotmail.fr>
2020-04-09 16:07:55 +0200

    header.php

--- php/view/presentation.php
+++ php/view/presentation.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/contact.css">
-    <title>biskot</title>
-</head>
-<body>
-    <header>
-    <section class="ancre">
-<a href="#Nos produits">Nos produits</a>
-<a href="#Nos Points De Ventes">Nos Points De Ventes</a>
-<a href="#contact">contact</a>
-<a href="#formulairedeconnexion">formulaire de connexion</a>
-</section>
-    <img class="logo" src="assets/img/LogoBiskot.png" alt="logo entreprise">
-    
 
-    </header>
-
-    <main>
 
     <h2>Presentation de l'entreprise</h2>
 <section class="presentation">
