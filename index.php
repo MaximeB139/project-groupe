3bc899c9da782affae62c44956435e1864f74936
Bmars <bapt_13@hotmail.fr>
2020-04-09 16:15:06 +0200

    modif header

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once "php/view/header.php";
 require_once "php/view/presentation.php";
 require_once "php/view/section-produits.php";
 require_once "php/view/points-ventes.php";
@@ -7,4 +8,4 @@ require_once "php/view/contact.php";
 require_once "php/view/footer.php";
 require_once "php/view/connection.php";
 require_once "php/view/deconnection.php";
-require_once "php/view/header.php";
+
